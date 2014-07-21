454992250b03f73b15352b8db879cd2e84c7a07b
Alex Grant <grantovich@users.noreply.github.com>
2014-07-21 11:05:47 -0400

    In-class work committed

--- demo.js
+++ demo.js
@@ -6,5 +6,49 @@ $(document).ajaxSend(function(event, xhr, settings) {
 });
 
 $(document).ready(function() {
-  // Your ajax code in here!
+  var baseUrl = 'http://jsonstub.com/articles';
+
+  $.ajax({
+    url: baseUrl
+  })
+  .done(function(data){
+    data.forEach(function(article){
+      var $article = $('<article>');
+      var $header = $('<h2>').addClass('title');
+      var articleUrl = baseUrl + '/' + article.id;
+      $header.append($('<a>').text(article.title).attr('href', articleUrl));
+      $article.append($header);
+      $article.append($('<p>').text(article.summary));
+      $('#articles').append($article);
+    });
+  })
+  .fail(function(jqXHR){
+    if(jqXHR.status === 404){
+      alert('Article not found!');
+    } else if(jqXHR.status === 500){
+      alert('Server error!');
+    }
+  });
+
+  $('#articles').on('click', '.title a', function(event){
+    $.ajax({
+      url: $(this).attr('href')
+    })
+    .done(function(data){
+      $('#article').empty().show();
+      $('#articles').hide();
+
+      var $backLink = $('<a>').text('Go back').attr('href', '#');
+      $backLink.click(function(event){
+        $('#article').hide();
+        $('#articles').show();
+      });
+
+      $('#article')
+        .append($('<h2>').text(data.title))
+        .append($('<p>').text(data.content))
+        .append($backLink);
+    });
+    event.preventDefault();
+  });
 });
